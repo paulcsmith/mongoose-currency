fbfdb16acaf9bdce8f806b1c9fdf4fb4b0508a76
Paul Smith <catalystmediastudios@gmail.com>
2013-04-18 23:16:55 -0600

    Better README. Adds new test that actually save to the databse and test returned value

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 ## Adds "Currency" as a type in for Mongoose Schemas
+
+```JavaScript
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var Currency = require('mongoose-currency').loadType(mongoose);
+
+var ProductSchema = Schema({
+  price: { type: Currency }
+});
+var Product = mongoose.model('Product', ProductSchema);
+
+var product = new Product({ price: "$1,200.55" });
+product.price; // Number: 1200.55
+```
+
+## Testing
+
+Make sure to have mocha installed `npm install mocha`
+
+in the root directory of the project run `mocha test`
--- test/test.js
+++ test/test.js
@@ -9,26 +9,62 @@ var ProductSchema = Schema({
 var Product = mongoose.model('Product', ProductSchema);
 
 describe('Currency Type', function () {
-  describe('setting a currency', function () {
-    it("should save correctly", function () {
-      var product = new Product({ price: "$10.55" });
-      product.price.should.equal(10.55);
-    })
-    it("should strip out dollar letters and return correct values", function () {
+  describe('the returned object from requiring mongoose-currency', function () {
+    it('should have a loadType method', function () {
+      var currencyModule = require('../index.js');
+      currencyModule.should.have.ownProperty('loadType');
+      currencyModule.loadType.should.be.a('function');
+    });
+  });
+
+  describe('setting a currency field and not saving the record', function () {
+    it("should strip out '$' and ','", function () {
+      var product = new Product({ price: "$1,000.55" });
+      product.price.should.equal(1000.55);
+    });
+    it("should strip out letters and return correct money value", function () {
       var product = new Product({ price: "HF1sdf0.55" });
       product.price.should.equal(10.55);
-    })
-    it("should work when there are no cents", function () {
+    });
+    it("should work as a string when there are no cents", function () {
       var product = new Product({ price: "500" })
       product.price.should.equal(500);
-    })
-    it("should convert whole number", function () {
+    });
+    it("should work as a string when there are cents", function () {
+      var product = new Product({ price: "500.67" })
+      product.price.should.equal(500.67);
+    });
+    it("should work with whole number", function () {
       var product = new Product({ price: 500})
       product.price.should.equal(500);
-    })
-    it("should convert number with decimal/cents", function () {
+    });
+    it("should work with a number with decimal/cents", function () {
       var product = new Product({ price: 500.55})
       product.price.should.equal(500.55);
-    })
-  })
+    });
+  });
+
+  describe('setting a currency field and saving the record', function () {
+    before(function () {
+      mongoose.connect('localhost', 'mongoose_currency_test');
+    });
+    after(function () {
+      mongoose.connection.db.dropDatabase();
+    });
+    it('should not round up and should return the correct value', function (done) {
+      var product = new Product({ price: "$1,000.78" });
+      product.save(function (err, new_product) {
+        new_product.price.should.equal(1000.78);
+        done();
+      });
+    });
+    it('should not round down and should return the correct value', function (done) {
+      var product = new Product({ price: "$1,000.05" });
+      product.save(function (err, new_product) {
+        new_product.price.should.equal(1000.05);
+        done();
+      });
+    });
+  });
+
 })
