9c8ea3b3f3b5915a8ab46d209b4d2008fe260778
Paul Smith <catalystmediastudios@gmail.com>
2013-04-18 21:26:08 -0600

    Two tests passing. If leave off cents it returns undefined!

---
+++ index.js
@@ -0,0 +1,33 @@
+var mongoose = require('mongoose')
+    , Schema = mongoose.Schema
+    , SchemaType = mongoose.SchemaType
+    , Types = mongoose.Types
+    , mongo = mongoose.mongo;
+
+function Currency(path, options) {
+  SchemaType.call(this, path, options);
+}
+
+// allow { required: true }
+Currency.prototype.checkRequired = function (val) {
+  return undefined !== value;
+}
+
+Currency.prototype.cast = function(val) {
+  var currencyAsString = val.toString();
+  var digitsAndDotRegex = /\d*\.\d{1,2}/;
+  currencyAsString = currencyAsString.replace(/\,+|[a-zA-Z]+/g, "");
+  var currency = digitsAndDotRegex.exec(currencyAsString)[0];
+  return currency * 1;
+};
+
+/*!
+ * inherits
+ */
+
+Currency.prototype.__proto__ = SchemaType.prototype;
+
+module.exports.loadType = function(mongoose) {
+  mongoose.SchemaTypes.Currency = Currency;
+  return mongoose.Types.Currency = Currency;
+};
---
+++ test/test.js
@@ -0,0 +1,26 @@
+var should = require('should');
+var mongoose = require('mongoose');
+var Currency = require('../index.js').loadType(mongoose);
+var Schema = mongoose.Schema;
+
+var ProductSchema = Schema({
+  price: { type: Currency }
+});
+var Product = mongoose.model('Product', ProductSchema);
+
+describe('Currency Type', function () {
+  describe('setting a currency', function () {
+    it("should save correctly", function () {
+      var product = new Product({ price: "$10.55" });
+      product.price.should.equal(10.55);
+    })
+    it("should strip out dollar letters and return correct values", function () {
+      var product = new Product({ price: "HF1sdf0.55" });
+      product.price.should.equal(10.55);
+    })
+    it("should work when there are no cents", function () {
+      var product = new Product({ price: "500" })
+      product.price.should.equal(500);
+    })
+  })
+})
