211ba63275cad1b5fd915ccaafab9cccc6c535da
Jason Sperske <jason@sperske.com>
2021-11-20 13:43:17 -0800

    Updated to latest mongoose and added better test dependencies.

--- index.js
+++ index.js
@@ -1,50 +1,40 @@
-'use strict';
-var mongoose = require('mongoose');
-var util = require('util');
+const mongoose = require('mongoose');
+const util = require('util');
 
-module.exports.loadType = function(mongoose) {
-  mongoose.Types.Currency = mongoose.SchemaTypes.Currency = Currency;
-  return Currency;
-};
+const REGEX_FIND_DIGITS_AND_DOT = /\d*\.\d{1,2}/;
+const REGEX_FIND_COMMAS_AND_LETTERS = /\,+|[a-zA-Z]+/g;
+const REGEX_FIND_NEGATIVE = /^-/;
 
 function Currency(path, options) {
-  mongoose.SchemaTypes.Number.call(this, path, options);
+  mongoose
+    .SchemaTypes
+    .Number
+    .call(this, path, options);
 }
 
 /*!
  * inherits
  */
-
 util.inherits(Currency, mongoose.SchemaTypes.Number);
 
 Currency.prototype.cast = function(val) {
-  if ( isType('String', val) ) {
-    var currencyAsString = val.toString();
-    var findDigitsAndDotRegex = /\d*\.\d{1,2}/;
-    var findCommasAndLettersRegex = /\,+|[a-zA-Z]+/g;
-    var findNegativeRegex = /^-/;
-    var currency;
-    currencyAsString = currencyAsString.replace(findCommasAndLettersRegex, "");
-    currency = findDigitsAndDotRegex.exec(currencyAsString + ".0")[0]; // Adds .0 so it works with whole numbers
-    if ( findNegativeRegex.test(currencyAsString) ) {
+  if (typeof val === 'string') {
+    let currencyAsString = val.toString();
+    currencyAsString = currencyAsString.replace(REGEX_FIND_COMMAS_AND_LETTERS, "");
+    const currency = REGEX_FIND_DIGITS_AND_DOT.exec(currencyAsString + ".0")[0]; // Adds .0 so it works with whole numbers
+    if (REGEX_FIND_NEGATIVE.test(currencyAsString)) {
       return (currency * -100).toFixed(0) * 1;
-    } else{
+    } else {
       return (currency * 100).toFixed(0) * 1;
     }
-  } else if ( isType('Number', val) ) {
+  } else if (typeof val === 'number') {
     return val.toFixed(0) * 1;
   } else {
     return new Error('Should pass in a number or string');
   }
 };
 
-/**
- * isType(type, obj)
- * Supported types: 'Function', 'String', 'Number', 'Date', 'RegExp',
- * 'Arguments'
- * source: https://github.com/jashkenas/underscore/blob/1.5.2/underscore.js#L996
- */
-
-function isType(type, obj) {
-  return Object.prototype.toString.call(obj) == '[object ' + type + ']';
+module.exports.loadType = function(mongoose) {
+  mongoose.Types.Currency = mongoose.SchemaTypes.Currency = Currency;
+  return Currency;
 }
--- test/test.js
+++ test/test.js
@@ -1,194 +1,292 @@
-var should = require('should');
-var mongoose = require('mongoose');
-var Currency = require('../index.js').loadType(mongoose);
-var Schema = mongoose.Schema;
+const {MongoMemoryServer} = require('mongodb-memory-server');
+const should = require('should');
+const mongoose = require('mongoose');
+const Currency = require('../index').loadType(mongoose);
 
-var ProductSchema = Schema({
-  price: { type: Currency }
+const ProductSchema = new mongoose.Schema({
+  price: {
+    type: Currency
+  }
 });
-var Product = mongoose.model('Product', ProductSchema);
+const Product = mongoose.model('Product', ProductSchema);
+const AdvancedModelSchema = new mongoose.Schema({
+  price: {
+    type: Currency,
+    required: true,
+    min: 0,
+    max: 200
+  }
+});
+const AdvancedModel = mongoose.model('AdvancedModel', AdvancedModelSchema);
 
-describe('Currency Type', function () {
-  describe('the returned object from requiring mongoose-currency', function () {
-    it('should have a loadType method', function () {
-      var currencyModule = require('../index.js');
-      currencyModule.should.have.ownProperty('loadType');
+describe('Currency Type', async () => {
+  describe('the returned object from requiring mongoose-currency', () => {
+    it('should have a loadType method', () => {
+      const currencyModule = require('../index.js');
+      currencyModule
+        .should
+        .have
+        .ownProperty('loadType');
       currencyModule.loadType.should.be.a.Function;
     });
   });
 
-  describe('mongoose.Schema.Types.Currency', function () {
-    before(function () {
-      var currencyModule = require('../index.js').loadType(mongoose);
+  describe('mongoose.Schema.Types.Currency', () => {
+    before(() => {
+      require('../index.js').loadType(mongoose);
     });
-    it('mongoose.Schema.Types should have a type called Currency', function () {
-      mongoose.Schema.Types.should.have.ownProperty('Currency');
+    it('mongoose.Schema.Types should have a type called Currency', () => {
+      mongoose
+        .Schema
+        .Types
+        .should
+        .have
+        .ownProperty('Currency');
     });
-    it('mongoose.Types should have a type called Currency', function () {
-      mongoose.Types.should.have.ownProperty('Currency');
+    it('mongoose.Types should have a type called Currency', () => {
+      mongoose
+        .Types
+        .should
+        .have
+        .ownProperty('Currency');
     });
-    it('should be a function', function () {
+    it('should be a function', () => {
       mongoose.Schema.Types.Currency.should.be.a.Function;
     });
-    it('should have a method called cast', function () {
+    it('should have a method called cast', () => {
       mongoose.Schema.Types.Currency.prototype.cast.should.be.a.Function;
     });
   });
 
-  describe('setting a currency field and not saving the record', function () {
-    it("should store positive as an integer by multiplying by 100", function () {
-      var product = new Product({ price: "$9.95" });
-      product.price.should.equal(995);
+  describe('setting a currency field and not saving the record', () => {
+    it("should store positive as an integer by multiplying by 100", () => {
+      const product = new Product({price: "$9.95"});
+      product
+        .price
+        .should
+        .equal(995);
     });
-    it("should store negative as an integer by multiplying by -100", function () {
-      var product = new Product({ price: "-$9.95" });
-      product.price.should.equal(-995);
+    it("should store negative as an integer by multiplying by -100", () => {
+      const product = new Product({price: "-$9.95"});
+      product
+        .price
+        .should
+        .equal(-995);
     });
-    it("should strip out '$' and ','", function () {
-      var product = new Product({ price: "$1,000.55" });
-      product.price.should.equal(100055);
+    it("should strip out '$' and ','", () => {
+      const product = new Product({price: "$1,000.55"});
+      product
+        .price
+        .should
+        .equal(100055);
     });
-    it("should strip out letters and return correct money value", function () {
-      var product = new Product({ price: "HF1sdf0.55" });
-      product.price.should.equal(1055);
+    it("should strip out letters and return correct money value", () => {
+      const product = new Product({price: "HF1sdf0.55"});
+      product
+        .price
+        .should
+        .equal(1055);
     });
-    it("should work as a string when there are no cents", function () {
-      var product = new Product({ price: "500" })
-      product.price.should.equal(50000);
+    it("should work as a string when there are no cents", () => {
+      const product = new Product({price: "500"});
+      product
+        .price
+        .should
+        .equal(50000);
     });
-    it("should work as a string when there are cents", function () {
-      var product = new Product({ price: "500.67" })
-      product.price.should.equal(50067);
+    it("should work as a string when there are cents", () => {
+      const product = new Product({price: "500.67"});
+      product
+        .price
+        .should
+        .equal(50067);
     });
-    it("should work with whole number", function () {
-      var product = new Product({ price: 500 })
-      product.price.should.equal(500);
+    it("should work with whole number", () => {
+      const product = new Product({price: 500});
+      product
+        .price
+        .should
+        .equal(500);
     });
-    it("should round passed in number if they are floating point nums", function () {
-      var product = new Product({ price: 500.55 })
-      product.price.should.equal(501);
+    it("should round passed in number if they are floating point nums", () => {
+      const product = new Product({price: 500.55});
+      product
+        .price
+        .should
+        .equal(501);
     });
-    it('should round when there are > two digits past decimal point', function () {
-      var product = new Product({ price: 500.5588 })
-      product.price.should.equal(501);
+    it('should round when there are > two digits past decimal point', () => {
+      const product = new Product({price: 500.5588});
+      product
+        .price
+        .should
+        .equal(501);
       product.price = "$500.41999";
-      product.price.should.equal(50041);
+      product
+        .price
+        .should
+        .equal(50041);
     });
-    it('should not round when adding', function () {
-      var product = new Product({ price: 119 })
-      var product2 = new Product({ price: 103 })
-      var sum = product.price + product2.price;
-      sum.should.equal(222);
+    it('should not round when adding', () => {
+      const product = new Product({price: 119});
+      const product2 = new Product({price: 103});
+      const sum = product.price + product2.price;
+      sum
+        .should
+        .equal(222);
     });
-    it('should accept negative currency as a String', function () {
-      var product = new Product({ price: "-$5,000.55" })
-      product.price.should.equal(-500055);
+    it('should accept negative currency as a String', () => {
+      const product = new Product({price: "-$5,000.55"});
+      product
+        .price
+        .should
+        .equal(-500055);
     });
-    it('should accept negative currency as a Number', function () {
-      var product = new Product({ price: -5000 })
-      product.price.should.equal(-5000);
+    it('should accept negative currency as a Number', () => {
+      const product = new Product({price: -5000});
+      product
+        .price
+        .should
+        .equal(-5000);
     });
   });
 
-  describe('setting a currency field and saving the record', function () {
-    before(function () {
-      mongoose.connect('localhost', 'mongoose_currency_test');
+  describe('using a schema with advanced options (required, min, max)', () => {
+    it(
+      'should pass validation when a price is set and field is required',
+      (done) => {
+        const record = new AdvancedModel();
+        record.price = 100.00;
+        record.validate((err) => {
+          should
+            .not
+            .exist(err);
+          done();
+        });
+      }
+    );
+    it(
+      'should fail validation when a price is NOT set and field is required',
+      (done) => {
+        const record = new AdvancedModel();
+        record.validate((err) => {
+          should.exist(err);
+          err
+            .errors
+            .should
+            .have
+            .property('price');
+          err
+            .errors
+            .price
+            .kind
+            .should
+            .equal('required');
+          done();
+        });
+      }
+    );
+    it(
+      'should pass validation when value is in between min and max values',
+      (done) => {
+        const record = new AdvancedModel();
+        record.price = 100;
+        record.validate((err) => {
+          should
+            .not
+            .exist(err);
+          done();
+        });
+      }
+    );
+    it('should fail validation when value is below min', (done) => {
+      const record = new AdvancedModel();
+      record.price = -100;
+      record.validate((err) => {
+        should.exist(err);
+        err
+          .errors
+          .should
+          .have
+          .property('price');
+        err
+          .errors
+          .price
+          .kind
+          .should
+          .equal('min');
+        done();
+      });
     });
-    after(function () {
-      mongoose.connection.db.dropDatabase();
+    it('should fail validation when value is higher than max', (done) => {
+      const record = new AdvancedModel();
+      record.price = 500;
+      record.validate((err) => {
+        should.exist(err);
+        err
+          .errors
+          .should
+          .have
+          .property('price');
+        err
+          .errors
+          .price
+          .kind
+          .should
+          .equal('max');
+        done();
+      });
     });
-    it('should not round up and should return the correct value', function (done) {
-      var product = new Product({ price: "$9.95" });
-      product.save(function (err, new_product) {
-        new_product.price.should.equal(995);
-        Product.findById(new_product.id, function (err, product){
-          product.price.should.equal(995);
+  });
+
+  const mongod = await MongoMemoryServer.create();
+  mongoose.connect(`${mongod.getUri()}mongoose_currency_test`);
+
+  describe('setting a currency field and saving the record', () => {
+    it('should not round up and should return the correct value', (done) => {
+      const product = new Product({price: "$9.95"});
+      product.save((err, new_product) => {
+        new_product
+          .price
+          .should
+          .equal(995);
+        Product.findById(new_product.id, (err, product) => {
+          product
+            .price
+            .should
+            .equal(995);
           done();
         });
       });
     });
-    it('should not round down and should return the correct value', function (done) {
-      var product = new Product({ price: "$1,000.19" });
-      product.save(function (err, new_product) {
-        new_product.price.should.equal(100019);
-        Product.findById(new_product.id, function (err, product){
-          product.price.should.equal(100019);
+    it('should not round down and should return the correct value', (done) => {
+      const product = new Product({price: "$1,000.19"});
+      product.save((err, new_product) => {
+        new_product
+          .price
+          .should
+          .equal(100019);
+        Product.findById(new_product.id, (err, product) => {
+          product
+            .price
+            .should
+            .equal(100019);
           done();
         });
       });
     });
-    it('should be able to store values and adding them together returns the correct value', function (done) {
-      var product1 = new Product({ price: 103 });
-      var product2 = new Product({ price: 103 });
-      product1.save(function (err, product) {
-        product2.save(function (err, product2) {
-          var sum = product1.price + product2.price;
-          sum.should.equal(206);
+    it('should be able to store values and adding them together', (done) => {
+      const product1 = new Product({price: 103});
+      const product2 = new Product({price: 103});
+      product1.save((err, product) => {
+        product2.save((err, product2) => {
+          const sum = product1.price + product2.price;
+          sum
+            .should
+            .equal(206);
           done();
         })
       });
     });
   });
-
-  describe('using a schema with advanced options (required, min, max)', function () {
-    before(function () {
-      var advancedSchema = Schema({
-        price: { type: Currency, required: true, min: 0, max: 200 }
-      })
-      mongoose.model('AdvancedModel', advancedSchema);
-    });
-
-    it('should pass validation when a price is set and field is required', function (done) {
-      var advancedModel = mongoose.model('AdvancedModel');
-      var record = new advancedModel();
-      record.price = 100.00;
-      record.validate(function (err) {
-        should.not.exist(err);
-        done();
-      });
-    });
-    it('should fail validation when a price is NOT set and field is required', function (done) {
-      var advancedModel = mongoose.model('AdvancedModel');
-      var record = new advancedModel();
-      record.validate(function (err) {
-        should.exist(err);
-        err.errors.should.have.property('price');
-        err.errors.price.kind.should.equal('required');
-        done();
-      });
-    });
-    it('should pass validation when value is in between min and max values', function (done) {
-      var advancedModel = mongoose.model('AdvancedModel');
-      var record = new advancedModel();
-      record.price = 100;
-      record.validate(function (err) {
-        should.not.exist(err);
-        done();
-      });
-    });
-    it('should fail validation when value is below min', function (done) {
-      var advancedModel = mongoose.model('AdvancedModel');
-      var record = new advancedModel();
-      record.price = -100;
-      record.validate(function (err) {
-        should.exist(err);
-        err.errors.should.have.property('price');
-        err.errors.price.kind.should.equal('min');
-        done();
-      });
-    });
-    it('should fail validation when value is higher than max', function (done) {
-      var advancedModel = mongoose.model('AdvancedModel');
-      var record = new advancedModel();
-      record.price = 500;
-      record.validate(function (err) {
-        should.exist(err);
-        err.errors.should.have.property('price');
-        err.errors.price.kind.should.equal('max');
-        done();
-      });
-    });
-
-  });
-
-})
+});
